26133d649597b17aa9745dff2799bfdfcece2a0e
EmersonJunior <emersonjr.eng@gmail.com>
2018-12-16 14:40:06 +0000

    implemented examples and code optimization

--- pycamb.py
+++ pycamb.py
@@ -1,5 +1,5 @@
 #!/home/emerson/Documentos/projects/pycambridge/app/bin/python
-#aqui abaixo o caminho para o python de seu ambiente virtual
+#aqui acima o caminho para o python de seu ambiente virtual
 import requests
 import sys
 import re
@@ -17,30 +17,42 @@ class Colour:
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 
+def yellow_line():
+    print(Colour.YELLOW + '======================================================================================================================================' + Colour.END)
 
 def printing_definitions(soup, tab_search, Colour, definition):
     #grabbing searched Word Header
     word_header = soup.find(class_="headword")
 
-
-    word_classfication = soup.find(class_="pos")
-
-
     #there has to be a variable on the id to concatenate with dataset- so the search is correctly done for the wished tab
     language_block = soup.find(id="dataset-"+tab_search)
+    
+    #parse only for examples category
+    if tab_search == 'examples':
+        yellow_line()
+        print(Colour.BLUE + language_block.find(class_='cpexamps-head').get_text() + Colour.END)
+        for egs in language_block.find_all(class_='eg'):
+            #matching example source to highligh with another color
+            p = re.compile('\n\n.*\n\n$')
+            m = p.findall(egs.get_text())
+            print (Colour.GREEN + re.sub('\n\n.*\n\n$',Colour.CYAN+Colour.BOLD+m[0],egs.get_text()) + Colour.END)
+            yellow_line()
+        sys.exit()
 
     #child block of definitions
     try:
         sense_block = language_block.find_all(class_="sense-block")
     except AttributeError:
-        print (Colour.RED + "Ops, there's no definitions for the selected category" + Colour.END)
+        print (Colour.RED + "Ops, the selected category has no defitions for this word" + Colour.END)
         sys.exit()
 
+    
     #not printing yet, maybe use a flag whether to print it or not?
     #extra_examps = sense_block.find(class_="extraexamps")
+        
 
     #looping through headers and definitions and printing
-    print(Colour.YELLOW +'======================================================================================================================================' + Colour.END)
+    yellow_line()
 
     print(Colour.CYAN + Colour.BOLD + word_header.get_text().upper() + Colour.END)
     if definition == "full":
@@ -54,10 +66,17 @@ def printing_definitions(soup, tab_search, Colour, definition):
                         if defs.findParent().findParent().attrs['class'][0] == 'phrase-block':
                             print (Colour.DARKCYAN + re.sub('[:.]','.\n',defs.findParent().findParent().get_text().capitalize()) + Colour.END)
                         else:
-                            print (Colour.GREEN +re.sub('[:.]','.\n',defs.get_text())+'\n' + Colour.END)
-                        print(Colour.YELLOW + '======================================================================================================================================' + Colour.END)
+                            #match in between the curly braces and highligh it
+                            p = re.compile('\[.*\]')
+                            m = p.findall(defs.get_text())
+                            mod1 = Colour.GREEN + re.sub('[:.]','.\n',defs.get_text())+'\n' + Colour.END
+                            mod2 = re.sub('\[ .* \]',Colour.RED+m[0]+Colour.END+Colour.GREEN,mod1)
+                            print(mod2)
+                        yellow_line()
+            #for those which have the above class commented, below is performed
             else:
                 for defs in sb.find_all(class_="def-block pad-indent"):
+                    #catch pos-header to print word kind (verb, noun, adjective...)
                     try:
                         print (Colour.PURPLE + Colour.BOLD + defs.findParent().findParent().findParent().findPreviousSibling().find(class_='pos').get_text())
                     except AttributeError:
@@ -66,23 +85,30 @@ def printing_definitions(soup, tab_search, Colour, definition):
                     if defs.findParent().findParent().attrs['class'][0] == 'phrase-block':
                         print (Colour.DARKCYAN + re.sub('[:.]','.\n',defs.findParent().findParent().get_text().capitalize()) + Colour.END)
                     else: 
+                        #normally printing definitions
                         print (Colour.GREEN +re.sub('[:.]','.\n',defs.get_text())+'\n' + Colour.END)
-                        print(Colour.YELLOW + '======================================================================================================================================' + Colour.END)
+                        yellow_line()
 
-    
+    #only first and main defitions for easy reading
     else:
-        #for txtblock in sense_block[0].find_all(class_="txt-block txt-block--alt2"):
         for txtblock in sense_block[0].find(class_="def-block pad-indent"):
             try:
+                #catch pos header class here as well
                 print (Colour.PURPLE + Colour.BOLD + txtblock.findParent().findParent().findParent().findParent().findPreviousSibling().find(class_='pos').get_text())
             except AttributeError:
                 pass
-            #print (Colour.CYAN + Colour.BOLD + txtblock.get_text().capitalize() + Colour.END)
-            print (Colour.GREEN + re.sub('[:.]','.\n',txtblock.get_text().capitalize() + '\n') + Colour.END)
-            #print (Colour.GREEN + re.sub('[:.]','.\n',sense_block[0].find(class_="def-block pad-indent").get_text()+'\n') + Colour.END)
-        print(Colour.YELLOW + '======================================================================================================================================' + Colour.END)
+            p = re.compile('\[.*\]')
+            m = p.findall(txtblock.get_text())
+            mod1 = Colour.GREEN + re.sub('[:.]','.\n',txtblock.get_text() + '\n') + Colour.END
+            #some definitions have no such curly braces blocks
+            try:
+                mod2 = re.sub('\[ .* \]',Colour.RED+m[0]+Colour.END+Colour.GREEN,mod1)
+                print(mod2)
+            except IndexError:
+                print(mod1)
+        yellow_line()
 
-    return '===========EOL============='
+    return 0
 
 
 
@@ -90,20 +116,19 @@ def printing_definitions(soup, tab_search, Colour, definition):
 def help():
     return Colour.GREEN + '''Usage ---> ''' + Colour.YELLOW + '''python pycamb.py word category ''' + Colour.PURPLE + Colour.BOLD + ''':full:''' + Colour.END + Colour.PURPLE  + '''
 :full is an optional option if you want all the meanings of the word:
-categories -> 'ingles' 'americano' 'business' 'collection' '''+ Colour.END
-
-
+categories -> 'ingles' 'americano' 'business' 'collection' 'exemplos' '''+ Colour.END
 
 
 if __name__ ==  '__main__':
-    #arg to specify which word is being searched
+    #specify which word is being searched
     word = sys.argv[1]
-    #if help, shows help usage
+
+    #shows help usage
     if word == '--help':
         print(help())
         sys.exit()
     
-    #arg to select either Ingles or Americano or Business or Exemplos or Collocations
+    #select either Ingles or Americano or Business or Exemplos or Collocations
     try:
         tab = sys.argv[2] 
     except IndexError:
@@ -146,7 +171,7 @@ if __name__ ==  '__main__':
         tab_search = 'combinations'
         printing_definitions(soup, tab_search, Colour, level_def)   
 
-    #case of using default ingles
+    #pratical case to use ingles as default
     elif tab == 'full':
         tab_search = 'cald4'
         printing_definitions(soup, tab_search, Colour, definition='full')   
